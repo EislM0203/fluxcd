3cca5b57c7fe06249e04a11774de8fa328548f59
Markus Eisl <markuseisl2000@gmail.com>
2025-10-24 11:46:43 +0000

    placeholder

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 
-# make plan-tf
+make plan-tf
 
-# make apply-tf \
-# 	update-packages \
-# 	install-longhorn-dependencies \
-# 	install-rke2-server \
-# 	install-rke2-agent \
-# 	cluster-readiness-check
+make apply-tf \
+	update-packages \
+	install-longhorn-dependencies \
+	install-rke2-server \
+	install-rke2-agent \
+	cluster-readiness-check
 
 kubectl apply --server-side --kustomize ./kubernetes/bootstrap/flux
 sops --decrypt kubernetes/bootstrap/flux/age-key.sops.yaml | kubectl apply -f -
 kubectl apply -f kubernetes/flux/vars/cluster-settings.yaml
-kubectl apply --server-side --kustomize ./kubernetes/flux/config
-
-kubectl create namespace cert-manager || true
-kubectl apply --server-side --kustomize ./kubernetes/apps/cert-manager
